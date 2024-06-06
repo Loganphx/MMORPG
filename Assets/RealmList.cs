be036895550ae511dda30754a3c5204f9fd3587a
Logan Ingram <loganphx1@gmail.com>
2024-06-06 16:24:19 -0500

    - Setup RealmSelection Scene - Created RealmList.cs to request all realms from the master server.

---
+++ Assets/RealmList.cs
@@ -0,0 +1,61 @@
+using System;
+using DefaultNamespace;
+using Microsoft.Extensions.DependencyInjection;
+using Networking.PackageParser.PackageImplementations;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
+
+public class RealmList : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private ClientManager _clientManager;
+    private ClientConnectionDispatcher _clientConnectionDispatcher;
+    
+    void Awake()
+    {
+        _clientManager = GameObject.Find("ClientManager").GetComponent<ClientManager>();
+        _clientConnectionDispatcher = GameContext.ServiceProvider.GetRequiredService<ClientConnectionDispatcher>();
+    }
+
+    private async void Start()
+    {
+        int realmIndex = 1;
+        
+        await RequestRealmList();
+    }
+
+    private async Awaitable RequestRealmList()
+    {
+        await Awaitable.BackgroundThreadAsync();
+        _clientConnectionDispatcher.SendPackage(new RealmListRequestPackage()
+        {
+            Region = "US"
+        });
+
+        var realmListResponsePackage = _clientConnectionDispatcher.WaitForPackage<RealmListResponsePackage>().Result;
+
+        Debug.Log($"Received {realmListResponsePackage.Realms.Count} Realms");
+        await Awaitable.MainThreadAsync();
+
+        int i = 0;
+        foreach (Transform realmOptionTransform in transform)
+        {
+            if (i >= realmListResponsePackage.Realms.Count)
+            {
+                realmOptionTransform.gameObject.SetActive(false);
+                continue;
+            }
+            var realmInfo = realmListResponsePackage.Realms[i];
+            
+            var realmButton = realmOptionTransform.GetComponent<Button>();
+            var realmName = realmOptionTransform.GetChild(0).GetComponent<TMP_Text>();
+            var tempIndex = realmInfo.RealmId;
+            realmButton.onClick.AddListener(() => _clientManager.Select_Realm(tempIndex));
+            realmName.SetText(realmInfo.RealmName); 
+            i++;
+        }
+    }
+}
